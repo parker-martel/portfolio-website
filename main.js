c247362e909306ed3e7818ea4a000c9d08f90f9b
parker-martel <parkermartel48@gmail.com>
2022-12-31 13:29:05 -0800

    Added linkedin button

--- main.js
+++ main.js
@@ -2,16 +2,19 @@
 let checkbox = document.querySelector('.light-dark-mode');
 let githubText = document.querySelector("#githubText");
 let githubLogo = document.querySelector("#githubLogo");
+let linkedin = document.querySelector("#linkedin");
 let html = document.querySelector('html');
 checkbox.addEventListener('change', () => {
   if (checkbox.checked) {
     html.setAttribute('data-theme', 'light');
     githubLogo.setAttribute('src','assets/images/github-mark.png');
     githubText.setAttribute('src','assets/images/GitHub_Logo.png');
+    linkedin.classList.add("colored")
   } else {
     html.setAttribute('data-theme', 'dark');
     githubLogo.setAttribute('src','assets/images/github-mark-white.png');
     githubText.setAttribute('src','assets/images/GitHub_Logo_White.png');
+    linkedin.classList.remove("colored")
   }
 });
 
