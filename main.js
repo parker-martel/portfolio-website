04cef745f09115403048d5a65e85baa609d2c87f
parker-martel <parkermartel48@gmail.com>
2023-11-15 00:43:42 -0800

    updated nav bar

--- main.js
+++ main.js
@@ -58,6 +58,7 @@ aboutmebtn.addEventListener('click', function(event) {
 window.addEventListener('scroll', function(){
   let nav = document.querySelector("nav");
   nav.classList.toggle("sticky", window.scrollY > 50);
+  nav.classList.toggle("shadow-md", window.scrollY > 50);
 })
 
 
